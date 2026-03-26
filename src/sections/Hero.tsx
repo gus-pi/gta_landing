236943ab9f3dc0424397fff9483b590b55b8c2c2
PieMenard <pinedo.m.gustavo@gmail.com>
2026-03-26 16:27:05 -0400

    setup hero components

---
+++ src/sections/Hero.tsx
@@ -0,0 +1,32 @@
+import ComingSoon from './ComingSoon';
+
+const Hero = () => {
+    return (
+        <section className="hero-section">
+            <div className="size-full mask-wrapper">
+                <img src="/images/hero-bg.webp" alt="hero-bg" className="scale-out" />
+                <img src="/images/hero-text.webp" alt="hero-logo" className="title-logo fade-out" />
+                <img
+                    src="/images/watch-trailer.png"
+                    alt="trailer"
+                    className="trailer-logo fade-out"
+                />
+                <div className="play-img fade-out">
+                    <img src="/images/play.png" alt="play" className="w-7 ml-1" />
+                </div>
+            </div>
+            <div>
+                <img
+                    src="/images/big-hero-text.svg"
+                    alt="logo"
+                    className="size-full object-cover mask-logo"
+                />
+            </div>
+            <div className="fake-logo-wapper">
+                <img src="/images/big-hero-text.svg" alt="logo" className="overlay-logo" />
+            </div>
+            <ComingSoon />
+        </section>
+    );
+};
+export default Hero;
---
+++ src/sections/NavBar.tsx
@@ -0,0 +1,9 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <img src="/images/nav-logo.svg" alt="nav logo" className="scale-90" />
+            <img src="images/menu.svg" className="w-10" />
+        </nav>
+    );
+};
+export default NavBar;
