5ecb68784824ab7343e9db0ddd1db68230495d00
PieMenard <pinedo.m.gustavo@gmail.com>
2026-03-30 10:48:17 -0400

    implement post card section

---
+++ src/sections/PostCard.tsx
@@ -0,0 +1,53 @@
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { useRef } from 'react';
+
+const PostCard = () => {
+    const videoRef = useRef<HTMLVideoElement | null>(null);
+
+    useGSAP(() => {
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.post-card',
+                start: 'top center',
+                end: 'bottom center',
+                scrub: true,
+            },
+        });
+        if (videoRef.current) {
+            videoRef.current.onloadedmetadata = () => {
+                if (!videoRef.current) return;
+                tl.to(
+                    videoRef.current,
+                    {
+                        currentTime: videoRef.current.duration,
+                        duration: 3,
+                        ease: 'power1.inOut',
+                    },
+                    '<',
+                );
+            };
+        }
+    });
+
+    return (
+        <section className="post-card">
+            <div className="animated-gradient-bg" />
+            <div className="post-card-wrapper group hover:rotate-1 hover:-[1.02] transition duration-700">
+                <img src="/images/overlay.webp" alt="overlay" />ç
+                <video
+                    ref={videoRef}
+                    src="/videos/postcard-vd.mp4"
+                    muted
+                    autoPlay
+                    playsInline
+                    preload="auto"
+                />
+                <button className="group-hover:bg-yellow transition duration-700">
+                    Explore Leonida Keys
+                </button>
+            </div>
+        </section>
+    );
+};
+export default PostCard;
